14a29782c75131d360f4222bb422801ab35e15a9
hamzahassan <hamzahassan835@gmail.com>
2019-11-29 16:53:40 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+   static Exponentiation exponentiation = new Exponentiation();
+    static ExtendedEuclidean extendedEuclidean = new ExtendedEuclidean();
+    public static void main(String[] args) {
+        extendedEuclidean.extendedEuclidean(10,5);
+        extendedEuclidean.extendedEuclidean(13,85);
+    }
+}
